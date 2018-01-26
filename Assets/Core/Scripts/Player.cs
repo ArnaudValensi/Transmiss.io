d21bb23c639281d0b0ce0947344d9c15d33049a5
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-01-26 22:53:16 +0100

    add base ground with navmesh and player/enemy

---
+++ Assets/Core/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float movementSpeed = 0.2f;
+	public float rotationSpeed = 1f;
+	public Transform cameraTransform;
+
+	CharacterController controller;
+
+	void Start() {
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		// Movement
+		Vector3 movement = new Vector3(
+			x * movementSpeed * Time.deltaTime,
+			0f,
+			y * movementSpeed * Time.deltaTime
+		);
+
+		controller.Move(movement);
+	}
+}
