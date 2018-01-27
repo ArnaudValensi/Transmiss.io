6b00f47693082a061d708260cc7454cd2468579f
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-01-27 22:39:57 +0100

    add a leader board controller

---
+++ Assets/Core/Scripts/LeaderBoard.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using System;
+
+public class LeaderBoard : MonoBehaviour {
+
+	[SerializeField] Dictionary<string, PlayerInfo> players;
+	[SerializeField] GameObject playerTextPrefab;
+	List<GameObject> playerTexts;
+	GameObject playersLayout;
+
+	void Start() {
+		players = new Dictionary<string, PlayerInfo>();
+		playerTexts = new List<GameObject>();
+
+		playersLayout = transform.Find("Panel/PlayersLayout").gameObject;
+
+		AddPlayer("Player 1", "00FF00FF", 30);
+		AddPlayer("Player 2", "0000FFFF", 10);
+		AddPlayer("Player 3", "FF0000FF", 20);
+		AddPlayer("Player 4", "00FF00FF", 50);
+		AddPlayer("Player 5", "00FF00FF", 40);
+	}
+
+	public void AddPlayer(string name, string color, int score) {
+		players.Add(name, new PlayerInfo(color, score));
+
+		GameObject newPlayerText = Instantiate(playerTextPrefab, playersLayout.transform);
+		playerTexts.Add(newPlayerText);
+
+		UpdateRank();
+	}
+
+	public void RemovePlayer(string name) {
+		players.Remove(name);
+		GameObject playerText = playerTexts.PopFirst();
+		Destroy(playerText);
+	}
+
+	public void SetPlayerScore(string name, int newScore) {
+		players[name].score = newScore;
+		UpdateRank();
+	}
+
+	void UpdateRank() {
+		var playersByScore = players
+			.OrderByDescending(pair => pair.Value.score)
+			.ToDictionary(pair => pair.Key, pair => pair.Value.color);
+
+		int i = 0;
+		foreach (var player in playersByScore) {
+			TextMeshProUGUI text = playerTexts[i].GetComponent<TextMeshProUGUI>();
+
+			text.SetText(String.Format("{0}. {1}", i + 1, player.Key));
+			text.color = HexToColor(player.Value);
+			i++;
+		}
+	}
+
+	Color HexToColor(string hex) {
+		byte r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+		byte g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+		byte b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+		return new Color32(r,g,b, 255);
+	}
+
+	class PlayerInfo {
+		public PlayerInfo(string color, int score) {
+			this.color = color;
+			this.score = score;
+		}
+
+		public string color;
+		public int score;
+	}
+}
