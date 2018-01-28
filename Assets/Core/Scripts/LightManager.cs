56301462cedfd738a334d06fd46ee4ea400ef128
thomasbaillysalins <thomasbaillysalins01@gmail.com>
2018-01-28 12:57:57 +0100

    add sounds and some graphs

---
+++ Assets/Core/Scripts/LightManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour {
+
+    public Light[] beatLights;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("LightBeat");
+	}
+
+    IEnumerator LightBeat()
+    {
+        while (true)
+        {
+            StartCoroutine("SingleBeat");
+            yield return new WaitForSeconds(0.4f);
+        }
+    }
+
+    IEnumerator SingleBeat()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < beatLights.Length; j++)
+            {
+                beatLights[j].intensity += 0.8f;
+            }
+            yield return null;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < beatLights.Length; j++)
+            {
+                beatLights[j].intensity -= 0.8f;
+            }
+            yield return null;
+        }
+    }
+}
